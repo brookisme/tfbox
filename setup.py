0dc3ac1213b6235a22747ff9333a018abb96fa80
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-06-25 09:25:42 -0700

    setup.py: package_data

--- setup.py
+++ setup.py
@@ -13,12 +13,15 @@ setup(
   url = 'https://github.com/brookisme/surface_water',
   download_url = 'https://github.com/brookisme/surface_water/tarball/0.1',
   keywords = ['python','tensorflow','model'],
-  include_package_data=True,
+  include_package_data=False,
   data_files=[
     (
       'config',[]
     )
   ],
+  package_data={
+    'sw.nn': ['configs/*.yaml']
+  },
   classifiers = [],
   entry_points={
       'console_scripts': [
