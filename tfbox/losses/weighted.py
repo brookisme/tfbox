8221563fecdd6bead6dad4803f3e769299a11a9f
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-07-22 21:10:40 -0700

    losses/callbacks/loaders to folders

--- tfbox/losses/weighted.py
+++ tfbox/losses/weighted.py
@@ -1,29 +1,6 @@
 import tensorflow as tf
 import tensorflow.keras.losses as losses
 import tensorflow.keras.backend as K
-#
-# CONSTANTS
-#
-DEFAULT_LOSS='categorical_crossentropy'
-DEFAULT_WEIGHTED_LOSS='weighted_categorical_crossentropy'
-
-
-
-#
-# MAIN
-#
-def get(loss_func=None,weights=None,**kwargs):
-    if not loss_func:
-        if weights:
-            loss_func=DEFAULT_WEIGHTED_LOSS
-            kwargs['weights']=weights
-        else:
-            loss_func=DEFAULT_LOSS
-    if isinstance(loss_func,str):
-        loss_func=LOSS_FUNCTIONS.get(loss_func,loss_func)
-    if not isinstance(loss_func,str):
-        loss_func=loss_func(**kwargs)
-    return loss_func
 
 
 
@@ -52,11 +29,3 @@ def weighted_categorical_crossentropy(weights=None,**kwargs):
             return tf.reduce_mean(weighted_losses,axis=[1,2])
     return _loss
 
-
-#
-# LOSS FUNCTION DICT
-#
-LOSS_FUNCTIONS={
-    'weighted_categorical_crossentropy': weighted_categorical_crossentropy,
-    'categorical_crossentropy': losses.CategoricalCrossentropy
-}
