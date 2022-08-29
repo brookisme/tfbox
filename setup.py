35dbe7c041790c0ebe61486803102942d034e0dc
Brookie Guzder-Williams <bguzder-williams@wri.org>
2022-08-29 15:13:07 -0700

    [0.0.0.92]

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     'tfbox.nn': 'tfbox/nn',
     'tfbox.utils': 'tfbox/utils'
   },
-  version = '0.0.0.91',
+  version = '0.0.0.92',
   description = 'tfbox: a collection of models and tools for tensorflow',
   author = 'Brookie Guzder-Williams',
   author_email = 'brook.williams@gmail.com',
